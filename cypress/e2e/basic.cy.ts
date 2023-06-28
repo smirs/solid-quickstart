78ae77cf42e8766b9b6be47fa6699f838099fc9e
Saeed Mirshekari <saeed.mirshekari@gmail.com>
2023-06-28 15:13:34 -0500

    Update basic.cy.ts

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -5,7 +5,7 @@ describe('empty spec', () => {
 
   it('display header text', () => {
     cy.get('h1')
-    .contains('Hello World!', { matchCase: false })
+    .contains('🏀 New Record: 358', { matchCase: false })
   })
 
   it('displays Click button', () => {
